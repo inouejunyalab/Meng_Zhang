f9ff0f5b6af3d346a4d82dd9cd7e77f6c64e4866
MengZhang <108038398+Zhang-CMS@users.noreply.github.com>
2024-05-07 17:19:12 +0900

    Update and rename lal_pinn_adp_ext.cpp to lal_anna_adp_ext.cpp

--- anna-gpu-lammps/bcc_fe/lib/lal_anna_adp_ext.cpp
+++ anna-gpu-lammps/bcc_fe/lib/lal_anna_adp_ext.cpp
@@ -12,17 +12,17 @@
 #include <cassert>
 #include <cmath>
 
-#include "lal_pinn_adp.h"
+#include "lal_anna_adp.h"
 
 using namespace std;
 using namespace LAMMPS_AL;
 
-static PINNADP<PRECISION, ACC_PRECISION> PINNADPMF;
+static ANNAADP<PRECISION, ACC_PRECISION> ANNAADPMF;
 
 /*---------------------------------------------------------------------
     allocate memory on host and device and copy constants to device
 ----------------------------------------------------------------------*/
-int pinn_adp_gpu_init(const int ntypes, const int inum, const int nall, 
+int anna_adp_gpu_init(const int ntypes, const int inum, const int nall, 
                       const int max_nbors, const double cell_size, 
                       int& gpu_mode, FILE* screen, const int maxspecial,
                       const int ntl, const int nhl, const int nnod, 
@@ -32,24 +32,24 @@ int pinn_adp_gpu_init(const int ntypes, const int inum, const int nall,
                       double** host_cutsq, int* host_map, double*** host_weight_all,
                       double*** host_bias_all, double *host_gadp_params, int &adp_size) {
 
-    PINNADPMF.clear();
-    gpu_mode = PINNADPMF.device->gpu_mode();
+    ANNAADPMF.clear();
+    gpu_mode = ANNAADPMF.device->gpu_mode();
 
-    double gpu_split = PINNADPMF.device->particle_split();
-    int first_gpu = PINNADPMF.device->first_device();                            
-    int last_gpu = PINNADPMF.device->last_device();                              
-    int world_me = PINNADPMF.device->world_me();                                 
-    int gpu_rank = PINNADPMF.device->gpu_rank();                                 
-    int procs_per_gpu = PINNADPMF.device->procs_per_gpu();                       
+    double gpu_split = ANNAADPMF.device->particle_split();
+    int first_gpu = ANNAADPMF.device->first_device();                            
+    int last_gpu = ANNAADPMF.device->last_device();                              
+    int world_me = ANNAADPMF.device->world_me();                                 
+    int gpu_rank = ANNAADPMF.device->gpu_rank();                                 
+    int procs_per_gpu = ANNAADPMF.device->procs_per_gpu();                       
 
     if (gpu_split != 1.0)
         return -8;
 
     adp_size = sizeof(PRECISION);                                                
-    PINNADPMF.device->init_message(screen, "pinn_adp/gpu", first_gpu, last_gpu); 
+    ANNAADPMF.device->init_message(screen, "anna_adp/gpu", first_gpu, last_gpu); 
 
     bool message = false;
-    if (PINNADPMF.device->replica_me() == 0 && screen)
+    if (ANNAADPMF.device->replica_me() == 0 && screen)
         message = true;
 
     if (message) {
@@ -58,13 +58,13 @@ int pinn_adp_gpu_init(const int ntypes, const int inum, const int nall,
     }
     int init_ok = 0;
     if (world_me == 0)
-        init_ok = PINNADPMF.init(ntypes, inum, nall, max_nbors, cell_size,
+        init_ok = ANNAADPMF.init(ntypes, inum, nall, max_nbors, cell_size,
                                  gpu_split, screen, maxspecial, ntl, nhl, 
                                  nnod, nsf, npsf, ntsf, nout, ngp, n_mu, n_lamb, 
                                  e_base, flagsym, flagact, host_cutsq, host_map, 
                                  host_weight_all, host_bias_all, host_gadp_params);
 
-    PINNADPMF.device->world_barrier();                                            
+    ANNAADPMF.device->world_barrier();                                            
     if (message)                                                                  
         fprintf(screen, "Done.\n");                                               
 
@@ -78,13 +78,13 @@ int pinn_adp_gpu_init(const int ntypes, const int inum, const int nall,
             fflush(screen);
         }
         if (gpu_rank == i && world_me != 0)
-            init_ok = PINNADPMF.init(ntypes, inum, nall, max_nbors, cell_size,
+            init_ok = ANNAADPMF.init(ntypes, inum, nall, max_nbors, cell_size,
                                      gpu_split, screen, maxspecial, ntl, nhl, 
                                      nnod, nsf, npsf, ntsf, nout, ngp, n_mu, n_lamb, 
                                      e_base, flagsym, flagact, host_cutsq, host_map, 
                                      host_weight_all, host_bias_all, host_gadp_params);
 
-        PINNADPMF.device->gpu_barrier();
+        ANNAADPMF.device->gpu_barrier();
         if (message)
             fprintf(screen, "Done.\n");
     }
@@ -92,15 +92,15 @@ int pinn_adp_gpu_init(const int ntypes, const int inum, const int nall,
         fprintf(screen, "\n");
 
     if (init_ok == 0)
-        PINNADPMF.estimate_gpu_overhead();
+        ANNAADPMF.estimate_gpu_overhead();
     return init_ok;
 }
 
-void pinn_adp_gpu_clear() {
-    PINNADPMF.clear();
+void anna_adp_gpu_clear() {
+    ANNAADPMF.clear();
 }
 
-int** pinn_adp_gpu_compute_n(const int ago, const int inum_full, const int nall, 
+int** anna_adp_gpu_compute_n(const int ago, const int inum_full, const int nall, 
                              const int nlocal, double** host_x, int* host_type, 
                              double* sublo, double* subhi, tagint* tag, 
                              int** nspecial, tagint** special, const bool eflag, 
@@ -109,12 +109,12 @@ int** pinn_adp_gpu_compute_n(const int ago, const int inum_full, const int nall,
                              void* ladp_params[], int& host_start, int** ilist, 
                              int** jnum, bool& success, const double cpu_time) {                   
 
-    return PINNADPMF.compute(ago, inum_full, nall, nlocal, host_x, host_type, 
+    return ANNAADPMF.compute(ago, inum_full, nall, nlocal, host_x, host_type, 
                              sublo, subhi, tag, nspecial, special, eflag, vflag, 
                              ea_flag, va_flag, adp_rho, adp_mu, adp_lambda, 
                              ladp_params, host_start, ilist, jnum, success, cpu_time);
 }
-void pinn_adp_gpu_compute(const int ago, const int inum_full, const int nall, 
+void anna_adp_gpu_compute(const int ago, const int inum_full, const int nall, 
                           const int nlocal, double** host_x, int* host_type, 
                           int* ilist, int* numj, int** firstneigh, 
                           const bool eflag, const bool vflag, 
@@ -122,18 +122,18 @@ void pinn_adp_gpu_compute(const int ago, const int inum_full, const int nall,
                           void* adp_mu[], void* adp_lambda[], void* ladp_params[], 
                           int& host_start, bool& success, const double cpu_time) {                 
 
-    PINNADPMF.compute(ago, inum_full, nall, nlocal, host_x, host_type, 
+    ANNAADPMF.compute(ago, inum_full, nall, nlocal, host_x, host_type, 
                       ilist, numj, firstneigh, eflag, vflag, ea_flag, 
                       va_flag, adp_rho, adp_mu, adp_lambda, ladp_params, 
                       host_start, success, cpu_time);
 }
 
-void pinn_adp_gpu_compute_force(const int nall, int* ilist, const bool eflag, 
+void anna_adp_gpu_compute_force(const int nall, int* ilist, const bool eflag, 
                                 const bool vflag, const bool ea_flag, const bool va_flag) {
 
-    PINNADPMF.compute_force(nall, ilist, eflag, vflag, ea_flag, va_flag);
+    ANNAADPMF.compute_force(nall, ilist, eflag, vflag, ea_flag, va_flag);
 }
 
-double pinn_adp_gpu_bytes() {
-    return PINNADPMF.host_memory_usage();
+double anna_adp_gpu_bytes() {
+    return ANNAADPMF.host_memory_usage();
 }
